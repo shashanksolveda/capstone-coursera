1b2473ef1e2f4c1b2f90a1beb8d1b52ed9a70eb9
Shashank Sharda <ssharda@salmonggn.com>
2023-12-14 22:48:51 +0530

    initial components added

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Route, Switch} from 'react-router-dom';
+import Homepage from './components/Homepage';
+import Booking from './components/Booking';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path='/' Component={Homepage}/>
+        <Route path='/booking' Component={Booking} />
+      </Switch>
     </div>
   );
 }
+
 
 export default App;
